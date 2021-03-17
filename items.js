7e433e781e2833c368bb0a8dde04dc179f97300f
ryuvi <vicenters10@gmail.com>
2021-03-17 15:04:26 -0300

    Added, Adjusted some functionalities

--- items.js
+++ items.js
@@ -1,9 +1,55 @@
-let list = document.getElementById("list");
+let helpers = document.getElementById("helpers");
+let upgrades = document.getElementById("upgrades");
 let multItems = [
   {
     name: "Multiplier +0.25",
     value: 0.25,
     buy: 10,
+  },
+  {
+    name: "Multiplier +5",
+    value: 5,
+    buy: 50,
+  },
+  {
+    name: "Multiplier +25",
+    value: 25,
+    buy: 250,
+  },
+  {
+    name: "Multiplier +125",
+    value: 125,
+    buy: 1250,
+  },
+  {
+    name: "Multiplier +625",
+    value: 625,
+    buy: 6250,
+  },
+  {
+    name: "Multiplier +3125",
+    value: 3125,
+    buy: 31250,
+  },
+  {
+    name: "Multiplier +15625",
+    value: 15625,
+    buy: 156250,
+  },
+  {
+    name: "Multiplier +78125",
+    value: 78125,
+    buy: 781250,
+  },
+  {
+    name: "Multiplier +390625",
+    value: 390625,
+    buy: 3906250,
+  },
+  {
+    name: "Multiplier +1953125",
+    value: 1953125,
+    buy: 19531250,
   },
 ];
 let touchItems = [
@@ -24,7 +70,7 @@ multItems.forEach(({ name, value, buy }) => {
         </button>
     `;
 
-  list.appendChild(li);
+  helpers.appendChild(li);
 });
 
 touchItems.forEach(({ name, value, buy }) => {
@@ -37,5 +83,5 @@ touchItems.forEach(({ name, value, buy }) => {
           </button>
       `;
 
-  list.appendChild(li);
+  upgrades.appendChild(li);
 });
